9ee9c8a64d4ca5dd02a54e513dd5889a344345bb
Petr Stepanek <petrstepanek99@proton.me>
2026-05-22 16:15:39 +0300

    Ci/CD fixz

--- internal/api/http/routes_play.go
+++ internal/api/http/routes_play.go
@@ -3,9 +3,6 @@ package http
 import "net/http"
 
 func (s *Server) routesPlay() {
-	if s.mux == nil {
-		s.mux = http.NewServeMux()
-	}
 	s.mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		_, _ = w.Write([]byte("ok"))
